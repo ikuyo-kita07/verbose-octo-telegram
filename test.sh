5a001025bbde0fcd3d684d7a1da367f1ce9ecfd6
—͟͞͞★ ɪᴋ𝘂ᵞο ᴋ𝖎𝘵𝕒 ★ <171214813+ikuyo-kita07@users.noreply.github.com>
2026-05-10 13:10:59 +0000

    yest

--- test.sh
+++ test.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
 
-sudo apt-get install curl
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-sudo apt-get install speedtest-cli 
-speedtest-cli
